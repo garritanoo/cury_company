7f666bf55dff560adce5f9eb84e17db76fd4109c
Juarez Garritano <garritanoo@gmail.com>
2023-07-06 10:01:53 -0300

    Alteração referente ao formato das datas na linha 158

--- pages/2_🏍_Visao_Entregadores.py
+++ pages/2_🏍_Visao_Entregadores.py
@@ -155,7 +155,7 @@ data_slider = st.sidebar.slider(
     value=pd.datetime(2022, 4, 13),
     min_value=pd.datetime(2022, 2, 11),
     max_value=pd.datetime(2022, 4, 6),
-    format='DD-MM-YYYY')
+    format='YYYY-MM-DD')
 
 traffic_options = st.sidebar.multiselect(
     'Quais as condições do transito?',
