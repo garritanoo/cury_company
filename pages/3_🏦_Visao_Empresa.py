a5f508d22a8139a6fb457b760b5b86b9efd71282
Juarez Garritano <garritanoo@gmail.com>
2023-07-06 11:45:03 -0300

    Alteração na página Visao_Empresa
    1. Inclusão do módulo datetime
    2. Inclusão do datetime para visualização do filtro de datas.

--- pages/3_🏦_Visao_Empresa.py
+++ pages/3_🏦_Visao_Empresa.py
@@ -217,9 +217,9 @@ st.markdown('---')
 st.sidebar.markdown('### Selecione a data limite')
 data_slider = st.sidebar.slider(
     'Até qual valor?',
-    value=pd.datetime(2022, 4, 13),
-    min_value=pd.datetime(2022, 2, 11),
-    max_value=pd.datetime(2022, 4, 6),
+    value=datetime(2022, 4, 13),
+    min_value=datetime(2022, 2, 11),
+    max_value=datetime(2022, 4, 6),
     format='DD-MM-YYYY')
 
 traffic_options = st.sidebar.multiselect(
